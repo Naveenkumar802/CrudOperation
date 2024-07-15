681baa3cfb17f1be5d71388e45eaa34b5e323621
hp <hp@KSVLAP062>
2024-07-15 16:19:32 +0530

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using CrudTask.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrudTask.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        { 
+
+        }
+        public DbSet<Employee> Employees { get; set; }
+
+    }
+}
